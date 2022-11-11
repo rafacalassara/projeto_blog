bf887b50c259defc10db1fa537920942739f35e5
Rafael Calassara <rafaelcalassara@hotmail.com>
2022-11-11 15:33:06 -0300

    adiciondo captcha

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,21 +1,40 @@
 from django.forms import ModelForm
 from .models import  Comentario
+import requests
 
 class FormComentario(ModelForm):
 
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        raw_data = self.data
+        
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LeIjvwiAAAAACpfReJ5ZOUakLw81YouEw3FjToW',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('sucess'):
+            self.add_error(
+                'comentario',
+                'Prove que você não é um robô.'
+            ) 
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
         if len(nome) < 4:
             self.add_error(
                 'nome_comentario',
                 'O nome precisa ter mais que 4 caracteres.'
             ) 
-
-        
+  
 
     class Meta:
         model = Comentario
